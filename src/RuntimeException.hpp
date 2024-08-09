51ff2d5cecf65855089fe5b683013b7701436f8e
astyles10 <alexanderstyles10@gmail.com>
2024-08-09 16:30:18 +1000

    Catch

--- src/RuntimeException.hpp
+++ src/RuntimeException.hpp
@@ -4,6 +4,8 @@
 #include <exception>
 
 // TODO: Log exceptions to an output stream - stdout, kafka, central logging, etc.
+// See https://stackoverflow.com/questions/52357/what-is-the-point-of-clog
+// This probably shouldn't be an exception handler though - what if we get an exception in the logger?
 class RuntimeException : public std::exception {
 
 };
